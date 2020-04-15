de6480d06c9ce5c64dc2dabba4cfa27e45b3d5d7
Bartek Pacia <barpac02@gmail.com>
2020-04-15 04:13:00 +0200

    updated package.json commands to conform to newest changes

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,21 @@ import * as discord from "./discord/bot_discord"
 import { TelegramConfig } from "./telegram/types_telegram"
 import { DiscordConfig } from "./discord/types_discord"
 
-console.log("Starting bot fleet...")
+let telegramRunning = false
+let discordRunning = false
 
-const telegramBot = new telegram.EnforcingTelegramBot(new TelegramConfig())
-const discordBot = new discord.EnforcingDiscordBot(new DiscordConfig())
+if (process.argv.includes("--telegram")) {
+  const telegramBot = new telegram.EnforcingTelegramBot(new TelegramConfig())
+  telegramRunning = true
+  console.log("Started Telegram bot.")
+}
+
+if (process.argv.includes("--discord")) {
+  const discordBot = new discord.EnforcingDiscordBot(new DiscordConfig())
+  discordRunning = true
+  console.log("Started Discord bot.")
+}
+
+if (!telegramRunning && !discordRunning) {
+  console.error("You must run bot for at least one platform!")
+}
