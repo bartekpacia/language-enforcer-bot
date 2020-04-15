ef4f55a8fa1fff93fc8a629e02be7764dca1a893
Bartek Pacia <barpac02@gmail.com>
2020-04-15 03:51:35 +0200

    refactored Discord bot to be OO

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
-import * as core from "./core/core"
-
 import * as telegram from "./telegram/bot_telegram"
-
 import * as discord from "./discord/bot_discord"
 import { TelegramConfig } from "./telegram/types_telegram"
+import { DiscordConfig } from "./discord/types_discord"
 
 console.log("Starting bot fleet...")
 
 const telegramBot = new telegram.EnforcingTelegramBot(new TelegramConfig())
-
-// This doesn't work like that, i'm nub
+const discordBot = new discord.EnforcingDiscordBot(new DiscordConfig())
