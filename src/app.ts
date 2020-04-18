c104b0a8665fba6dc0efe6da3487efa6ff87b796
Bartek Pacia <barpac02@gmail.com>
2020-04-18 18:15:58 +0200

    fixed retrieving secrets

--- src/app.ts
+++ src/app.ts
@@ -15,31 +15,44 @@ export const config = new CoreConfig()
 
 admin.initializeApp()
 
+const TELEGRAM_TOKEN_NAME = "projects/telegram-lang-enforcer/secrets/TELEGRAM_TOKEN/versions/latest"
+const TELEGRAM_TOKEN_NAME_DEV = "projects/telegram-lang-enforcer/secrets/TELEGRAM_TOKEN_DEV/versions/latest"
+const DISCORD_TOKEN_NAME = "projects/telegram-lang-enforcer/secrets/DISCORD_TOKEN/versions/latest"
+
 const secretClient = new SecretManagerServiceClient()
 
-async function main() {
+async function main(): Promise<void> {
   const core = new Core(config, new Translator())
 
-  async function getSecret(): Promise<string | null | undefined> {
-    const [version] = await secretClient.accessSecretVersion({ name: "TELEGRAM_TOKEN" })
+  async function getSecret(secretName: string): Promise<string | null | undefined> {
+    const [version] = await secretClient.accessSecretVersion({ name: secretName })
 
     return version.payload?.data?.toString()
   }
 
-  const TELEGRAM_TOKEN = await getSecret()
-  console.log(TELEGRAM_TOKEN)
-
   const telegramRunning = process.argv.includes("--telegram")
   const discordRunning = process.argv.includes("--discord")
+  const isDevMode = process.argv.includes("--dev")
+
+  const TELEGRAM_TOKEN = await getSecret(isDevMode ? TELEGRAM_TOKEN_NAME_DEV : TELEGRAM_TOKEN_NAME)
+  const DISCORD_TOKEN = await getSecret(DISCORD_TOKEN_NAME)
 
   if (telegramRunning) {
-    const telegramBot = new telegram.EnforcingTelegramBot(core, new TelegramConfig())
-    console.log("Started Telegram bot.")
+    if (TELEGRAM_TOKEN != null) {
+      const telegramBot = new telegram.EnforcingTelegramBot(core, new TelegramConfig(TELEGRAM_TOKEN))
+      console.log("Started Telegram bot.")
+    } else {
+      console.error("TELEGRAM_TOKEN is null/undefined!")
+    }
   }
 
   if (discordRunning) {
-    const discordBot = new discord.EnforcingDiscordBot(core, new DiscordConfig())
-    console.log("Started Discord bot.")
+    if (DISCORD_TOKEN != null) {
+      const discordBot = new discord.EnforcingDiscordBot(core, new DiscordConfig(DISCORD_TOKEN))
+      console.log("Started Discord bot.")
+    } else {
+      console.error("DISCORD_TOKEN is null/undefined")
+    }
   }
 
   if (!telegramRunning && !discordRunning) {
